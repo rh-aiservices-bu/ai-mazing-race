97779bf5dbbe6ad370493c1caa04bbda4d7db22c
RHRolun <rlundber@redhat.com>
2025-01-08 12:00:49 +0100

    Updated instructions and fixing pre-processing

--- lab-materials/07/support_functions/model_training.py
+++ lab-materials/07/support_functions/model_training.py
@@ -12,36 +12,61 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
-def pre_process_data(df, input_features, target):
-    items_to_scale = ['price', 'old_price', 'width', 'height', 'depth', 'discount_amount', 'size']
-    items_to_encode = ['sellable_online', 'other_colors']
-    items_to_one_hot_encode = ['category']
-    
+def fit_scalers_and_encoders(df, input_features, target):
+    features_to_scale = ['price', 'old_price', 'width', 'height', 'depth', 'discount_amount', 'size']
+    features_to_encode = ['sellable_online', 'other_colors']
+
     scalers = {}
     encoders = {}
     
-    for i in items_to_scale:
+    for i in features_to_scale:
         if i in input_features or i == target:
             scalers[i] = MinMaxScaler()
             df[[i]] = scalers[i].fit_transform(df[[i]])
-        else:
-            df = df.drop(i, axis=1)
     
-    for i in items_to_encode:
+    for i in features_to_encode:
         if i in input_features or i == target:
             encoders[i] = LabelEncoder()
             df[i] = encoders[i].fit_transform(df[i])
-        else:
-            df = df.drop(i, axis=1)
-    
-    for i in items_to_one_hot_encode:
-        if i in input_features or i == target:
-            df = pd.get_dummies(df, columns=[i])
-        else:
-            df = df.drop(i, axis=1)
-    
-    df = df.drop(["name", "short_description"], axis=1)
+
     return df, scalers, encoders
+
+def apply_scalers_and_encoders(df, scalers, encoders):
+    for k, v in scalers.items():
+        if k in df.columns:
+            df[[k]] = v.transform(df[[k]])
+
+    for k, v in encoders.items():
+        if k in df.columns:
+            print(k)
+            df[k] = v.transform(df[k])
+
+    return df
+
+def one_hot_encode_dataset(df_train, df_test, input_features, target):
+    features_to_one_hot_encode = ['category']
+
+    for i in features_to_one_hot_encode:
+        if i in input_features or i == target:
+            df_train_encoded = pd.get_dummies(df_train, columns=[i])
+            df_test_encoded = pd.get_dummies(df_test, columns=[i])
+            df_test_encoded = df_test_encoded.reindex(columns=df_train_encoded.columns, fill_value=0)
+
+    return df_train_encoded, df_test_encoded
+
+def pre_process_data(df_train, df_test, input_features, target):
+    df_train, df_test = one_hot_encode_dataset(df_train, df_test, input_features, target)
+    df_train, scalers, encoders = fit_scalers_and_encoders(df_train, input_features, target)
+    df_test = apply_scalers_and_encoders(df_test, scalers, encoders)
+    
+    return df_train, df_test, scalers, encoders
+
+def drop_unused_columns(df, input_features, target):
+    for i in df.columns:
+        if i not in input_features and i!=target:
+            print(f"dropping {i}")
+            df = df.drop(i, axis=1)
+    return df
 
 def create_model(my_learning_rate, input_size):
     model = tf.keras.Sequential([
@@ -71,8 +96,12 @@ def save_model(model_name, model, df_train, df_test, scalers, encoders):
     df_test.to_parquet(f"{model_name}/X_test.parquet")
 
 def train_model(df, input_features, target, split_function, model_name):
-    df, scalers, encoders = pre_process_data(df, input_features, target)
-    df_train, df_test = split_function(df)
+    df = drop_unused_columns(df, input_features, target)
+    X_train, X_test, y_train, y_test = split_function(df)
+    df_train = pd.concat([X_train, y_train], axis=1)
+    df_test = pd.concat([X_test, y_test], axis=1)
+
+    df_train, df_test, scalers, encoders = pre_process_data(df_train, df_test, input_features, target)
 
     tf.keras.utils.set_random_seed(112)
 
@@ -100,4 +129,4 @@ def train_model(df, input_features, target, split_function, model_name):
     save_model(model_name, model, df_train, df_test, scalers, encoders)
     print(f"Model and artifacts saved to folder {model_name}/")
 
-    return df
+    return df_train, df_test
